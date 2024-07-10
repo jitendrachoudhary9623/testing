a69cb243b8527fdc38067d4b55d9953973e75990
Jitendra Choudhary <jitendra93266@gmail.com>
2024-07-10 12:58:42 +0530

    Update nescafe-trial.js

--- nescafe-trial.js
+++ nescafe-trial.js
@@ -6,18 +6,24 @@
     function injectStyles() {
         var style = document.createElement('style');
         style.textContent = `
+            #${scriptId} {
+                position: fixed;
+                bottom: 20px;
+                right: 20px;
+                z-index: 9999;
+            }
             .gwd-page-container { position: relative; width: 100%; height: 100%; }
             .gwd-page-content { position: absolute; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
             .gwd-page-wrapper { position: absolute; transform: translateZ(0); }
             .banner { width: 300px; height: 250px; }
             .expanded { width: 480px; height: 250px; }
-            .expand-button { position: absolute; width: 200px; height: 200px; top: 100%; left: 100%; transform: translate(-100%, -100%) translate3d(-60px, -70px, 0px); }
-            .close-button { position: absolute; width: 120px; height: 48px; background-color: rgba(153, 153, 153, 0.2); top: 0; opacity: 0; left: 100%; transform: translate(-100%, 0%); }
-            .gwd-image-kyh7 { position: absolute; width: 200px; height: 200px; top: 100%; left: 100%; transform: translate(-100%, -100%) translate3d(-60px, -70px, 0px) scale3d(0.001, 0.001, 1); }
-            .gwd-image-16ed { position: absolute; width: 200px; height: 200px; left: 100%; top: 100%; transform: translate(-100%, -100%) translate3d(-50px, 190px, 0px) rotateZ(50deg); }
+            .expand-button { position: absolute; width: 200px; height: 200px; top: 100%; left: 100%; transform: translate(-100%, -100%) translate3d(-60px, -70px, 0px); cursor: pointer; }
+            .close-button { position: absolute; width: 120px; height: 48px; background-color: rgba(153, 153, 153, 0.2); top: 0; opacity: 0; left: 100%; transform: translate(-100%, 0%); cursor: pointer; }
+            .gwd-image-kyh7 { position: absolute; width: 200px; height: 200px; top: 100%; left: 100%; transform: translate(-100%, -100%) translate3d(-60px, -70px, 0px) scale3d(0.001, 0.001, 1); transition: transform 0.5s; }
+            .gwd-image-16ed { position: absolute; width: 200px; height: 200px; left: 100%; top: 100%; transform: translate(-100%, -100%) translate3d(-50px, 190px, 0px) rotateZ(50deg); transition: transform 0.3s; }
             .gwd-video-1pw6 { position: absolute; height: 100%; width: 100%; left: 50%; top: 50%; transform: translate(-50%, -50%); visibility: hidden; }
             .gwd-image-13xx { position: absolute; width: 100%; height: 100%; left: 50%; top: 50%; transform: translate(-50%, -50%); opacity: 1; }
-            .gwd-image-1tz2 { position: absolute; width: 16px; height: 16px; left: 100%; top: 0; transform: translate(-100%, 0%) translate3d(-20px, 20px, 0px); }
+            .gwd-image-1tz2 { position: absolute; width: 16px; height: 16px; left: 100%; top: 0; transform: translate(-100%, 0%) translate3d(-20px, 20px, 0px); cursor: pointer; }
         `;
         document.head.appendChild(style);
     }
@@ -69,6 +75,7 @@
         
         document.getElementById('expand-button').addEventListener('click', handleExpand);
         document.getElementById('close-button').addEventListener('click', handleClose);
+        document.getElementById('b_close').addEventListener('click', handleClose);
         
         // Animation for the banner page
         setTimeout(() => {
